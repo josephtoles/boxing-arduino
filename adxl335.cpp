f73e505003ed74c12f947b72516616ed82e96f5c
josephtoles <jbtoles@gmail.com>
2015-11-03 15:15:53 -0800

    Punch detection works decently via integration

--- adxl335.cpp
+++ adxl335.cpp
@@ -6,21 +6,27 @@ const int X_PLUS = 397;
 const int X_MINUS = 264;
 const int Y_PLUS = 399;
 const int Y_MINUS = 264;
-const int Z_PLUS = 405;  // Requires slightly finer calibration
-const int Z_MINUS = 272;  // Requires slightly finer calibration
+const int Z_PLUS = 404;  // Requires slightly finer calibration
+const int Z_MINUS = 269;  // Requires slightly finer calibration
 
 // Variables for export
-int e_x;
-int e_y;
-int e_z;
+int n_1;
+int n_2;
+int n_3;
+int n_4;
+
+// Variables for remembering time
+int time_since_punch_start;
+bool punch_confirmed;
 
 void adxl335_setup() {
-  // Nothing to see here
+  time_since_punch_start = 0;
+  punch_confirmed = false;
 }
 
 
 void print_to_size(int number) {
-  Serial.print("  ");Serial.print(number);
+  Serial.print("");Serial.print(number);
   // You [Joseph] are an idiot.
   
   //char buffer[5] = "";
@@ -52,6 +58,7 @@ void adxl335_loop() {
   print_to_size(x);Serial.print(F("=ax"));
   print_to_size(y);Serial.print(F("=ay"));
   print_to_size(z);Serial.print(F("=az"));
+  Serial.println("");
 
   // X_PLUS is the value of X shen accelerating upward
   // X_MINUX is the value of X shen accelerating downward
@@ -66,11 +73,45 @@ void adxl335_loop() {
   Serial.print(" |");
   */
 
-  //int norm_squared = xs*xs + ys*ys + zs*zs;
   int n = (int) (sqrt((xs * xs) + (ys * ys) + (zs * zs)));
+  if (false) {
+    print_to_size(n);Serial.print(F(""));
+    Serial.println("");
+  }
 
-  print_to_size(n);Serial.print(F(""));
-  Serial.println("");
+  //Trying integration
+  if (n > 100) {
+    if (n > 145) {
+      punch_confirmed = true;
+    }
+    time_since_punch_start += n - 100;
+  } else {
+    if (punch_confirmed && (time_since_punch_start > 3000)) {
+      Serial.println(time_since_punch_start);
+      delay(1000);
+    }
+    time_since_punch_start = 0;
+    punch_confirmed = false;
+  }
+
+  // Trying hard cutoffs
+  /*
+  time_since_punch_start += 1;
+  if (punch_confirmed) {
+    if (n < 100) {
+      //Serial.println(time_since_punch_start);
+      time_since_punch_start = 0;
+      punch_confirmed = false;
+    }
+  } else {
+    if (n < 100) {
+      punch_confirmed = false;
+      time_since_punch_start = 0;
+    } else if (n > 145) {
+      punch_confirmed = true;
+    }
+  }
+  */
 
   // This code doesn't function on the Arduino for some reason
   /*
