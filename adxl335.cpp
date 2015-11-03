00e57cc31b948a807c5e6415731a9a87ab06a2bf
josephtoles <jbtoles@gmail.com>
2015-11-02 18:41:12 -0800

    Fixed bizarre bug (not actually so bizarre)

--- adxl335.cpp
+++ adxl335.cpp
@@ -6,8 +6,8 @@ const int X_PLUS = 397;
 const int X_MINUS = 264;
 const int Y_PLUS = 399;
 const int Y_MINUS = 264;
-const int Z_PLUS = 145;
-const int Z_MINUS = 15;
+const int Z_PLUS = 405;  // Requires slightly finer calibration
+const int Z_MINUS = 272;  // Requires slightly finer calibration
 
 // Variables for export
 int e_x;
@@ -15,14 +15,15 @@ int e_y;
 int e_z;
 
 void adxl335_setup() {
-  
+  // Nothing to see here
 }
 
 
 void print_to_size(int number) {
-  char buffer[6] = "";
-  sprintf(buffer, "%6d", number);
-  Serial.print(buffer);
+  //char buffer[5] = "";
+  //sprintf(buffer, "%5d", number);
+  //Serial.print(buffer);
+  Serial.print("  ");Serial.print(number);
 }
 
 
@@ -30,32 +31,43 @@ void print_to_size(int number) {
 int scale(int number, int minus, int plus) {
   double a = 200 * 1.0 / (plus - minus);
   double b = 100 - a * plus;
-  return (int) (a * number + b);
+  int output = (int) (a * number + b);
+  return output;
 }
 
 
 void adxl335_loop() {
+
   int x = analogRead(1);
   int y = analogRead(2);
   int z = analogRead(3);
 
+  int xs = scale(x, X_MINUS, X_PLUS);
+  int ys = scale(y, Y_MINUS, Y_PLUS);
+  int zs = scale(z, Z_MINUS, Z_PLUS);
+
+  print_to_size(x);Serial.print(F("=ax"));
+  print_to_size(y);Serial.print(F("=ay"));
+  print_to_size(z);Serial.print(F("=az"));
+
   // X_PLUS is the value of X shen accelerating upward
   // X_MINUX is the value of X shen accelerating downward
-  
-  x = scale(x, X_MINUS, X_PLUS);
-  y = scale(y, Y_MINUS, Y_PLUS);
-  z = scale(z, Z_MINUS, Z_PLUS);
 
-  print_to_size(x);Serial.print(F("=x"));
-  print_to_size(y);Serial.print(F("=y"));
-  print_to_size(z);Serial.print(F("=z"));
+  Serial.print(" |");
+
+  print_to_size(xs);Serial.print(F("=xs"));
+  print_to_size(ys);Serial.print(F("=ys"));
+  print_to_size(zs);Serial.print(F("=zs"));
+  //Serial.println("");
+
+  Serial.print(" |");
+
+  int norm_squared = xs*xs + ys*ys + zs*zs;
+  //int n = (int) (sqrt((z * z) + (y * y) + (z * z)));
+  //n = 4;
+
+  print_to_size(norm_squared);Serial.print(F("=n"));
   Serial.println("");
-
-  /*
-  e_x = x;
-  e_y = y;
-  e_z = z;
-  */
 
   // This code doesn't function on the Arduino for some reason
   /*
